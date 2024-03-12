d8bff304fb7cb41496aadd2033c5b12e6dee9232
Twine <64853635+Twelvet-Spark@users.noreply.github.com>
2024-03-12 12:24:47 +0500

    ...

--- lib/bottomMenu.dart
+++ lib/bottomMenu.dart
@@ -6,12 +6,15 @@ import 'package:geolocator/geolocator.dart';
 import 'package:naliv_delivery/misc.dart';
 import 'package:naliv_delivery/misc/api.dart';
 import 'package:naliv_delivery/misc/colors.dart';
+import 'package:naliv_delivery/pages/addressesPage.dart';
 import 'package:naliv_delivery/pages/businessSelectStartPage.dart';
 import 'package:naliv_delivery/pages/cartPage.dart';
 import 'package:naliv_delivery/pages/favPage.dart';
 import 'package:naliv_delivery/pages/homePage.dart';
+import 'package:naliv_delivery/pages/loginPage.dart';
 import 'package:naliv_delivery/pages/profilePage.dart';
 import 'package:naliv_delivery/pages/searchPage.dart';
+import 'package:naliv_delivery/pages/settingsPage.dart';
 import 'package:naliv_delivery/shared/commonAppBar.dart';
 
 import 'main.dart';
@@ -279,6 +282,16 @@ class _BottomMenuState extends State<BottomMenu> {
     });
   }
 
+  Map user = {};
+  Future<void> _getUser() async {
+    Map<String, dynamic>? user = await getUser();
+    if (user != null) {
+      setState(() {
+        user = user;
+      });
+    }
+  }
+
   @override
   void initState() {
     // TODO: implement initState
@@ -287,6 +300,7 @@ class _BottomMenuState extends State<BottomMenu> {
     _getLastSelectedBusiness();
     _getBusinesses();
     _setAppbars(context);
+    _getUser();
 
     setState(() {});
   }
@@ -391,13 +405,240 @@ class _BottomMenuState extends State<BottomMenu> {
 
   Drawer _drawer() {
     return Drawer(
-        width: 200,
         child: Column(
-          children: [
-            Container(
-              child: Placeholder(),
-            )
-          ],
-        ));
+      children: [
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              children: [
+                CircleAvatar(
+                  radius: MediaQuery.of(context).size.width * 0.10,
+                  backgroundImage: const NetworkImage(
+                      "https://air-fom.com/wp-content/uploads/2018/06/real_1920.jpg"),
+                ),
+                const SizedBox(
+                  height: 10,
+                  width: 10,
+                  child: Placeholder(),
+                ),
+                // SizedBox(
+                //   width: MediaQuery.of(context).size.width * 0.3,
+                //   child: Column(
+                //     crossAxisAlignment: CrossAxisAlignment.start,
+                //     children: [
+                //       Text(
+                //         user["name"] ?? "",
+                //         style: const TextStyle(
+                //             color: Colors.black,
+                //             fontWeight: FontWeight.w500,
+                //             fontSize: 16),
+                //       ),
+                //       Text(
+                //         user["login"] ?? "",
+                //         style: const TextStyle(
+                //             color: Colors.black,
+                //             fontWeight: FontWeight.w400,
+                //             fontSize: 14),
+                //       ),
+                //       Text(
+                //         user["user_id"] ?? "",
+                //         style: TextStyle(
+                //             color: Colors.grey.shade400,
+                //             fontWeight: FontWeight.w400,
+                //             fontSize: 14),
+                //       )
+                //     ],
+                //   ),
+                // )
+              ],
+            ),
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+          child: Column(
+            children: [
+              TextButton(
+                style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 30)),
+                onPressed: () {},
+                child: const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.shopping_bag_outlined,
+                      size: 24,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "История заказов",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              const Divider(),
+              TextButton(
+                style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 30)),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const AddressesPage()),
+                  );
+                },
+                child: const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.home_outlined,
+                      size: 24,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Адреса доставки",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              const Divider(),
+              TextButton(
+                style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 30)),
+                onPressed: () {},
+                child: const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.credit_card,
+                      size: 24,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Карты оплаты",
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              const Divider(),
+              TextButton(
+                style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 30)),
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const SettingsPage();
+                    },
+                  ));
+                },
+                child: const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.settings_outlined,
+                      size: 24,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Настройки",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              const Divider(),
+              TextButton(
+                style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 30)),
+                onPressed: () {},
+                child: const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.chat_bubble_outline,
+                      size: 24,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Поддержка",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+              const Divider(),
+              TextButton(
+                style: TextButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 30)),
+                onPressed: () {
+                  print(123);
+                  logout();
+                  Navigator.pushReplacement(context, MaterialPageRoute(
+                    builder: (context) {
+                      return const LoginPage();
+                    },
+                  ));
+                },
+                child: const Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Icon(
+                      Icons.exit_to_app_outlined,
+                      size: 24,
+                      color: Colors.black,
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Text(
+                      "Выйти",
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w400,
+                          fontSize: 20),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
+        )
+      ],
+    ));
   }
 }
