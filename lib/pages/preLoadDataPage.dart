d264c1bd275111b4ee5960a82c51f58649e6f776
breadwithmeth <sergeq2222@gmail.com>
2024-06-24 19:18:06 +0500

    +

--- lib/pages/preLoadDataPage.dart
+++ lib/pages/preLoadDataPage.dart
@@ -69,17 +69,16 @@ class _PreLoadDataPageState extends State<PreLoadDataPage> {
     // TODO: implement initState
     super.initState();
     FlutterView view = WidgetsBinding.instance.platformDispatcher.views.first;
-    Size size = view.physicalSize;
+    Size size = view.physicalSize / view.devicePixelRatio;
+    // MediaQuery.textScalerOf(context).scale(fontSize)
     double width = size.width;
     double height = size.height;
     if (size.aspectRatio < 1) {
-      globals.scaleParam = (height / 1920) *
-          (width / 1080) *
-          .4;
+      // globals.scaleParam = (height / 1920) * (width / 1080) * .4;
+      globals.scaleParam = 0.4;
     } else {
-      globals.scaleParam = (height / 1080) *
-          (width / 1920) *
-          1;
+      globals.scaleParam = 0.4;
+      // globals.scaleParam = (height / 1080) * (width / 1920) * 1;
     }
 
     // globals.scaleParam = 1;
