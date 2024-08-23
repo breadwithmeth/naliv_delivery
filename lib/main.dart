a0cddf3738e7f2f835a3836aa9fc40d83cb2fd7d
breadwithmeth <sergeq2222@gmail.com>
2024-08-23 21:16:01 +0500

    +

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:naliv_delivery/pages/paintLogoPage.dart';
 import '../globals.dart' as globals;
 import 'package:naliv_delivery/misc/api.dart';
@@ -12,11 +13,35 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:workmanager/workmanager.dart';
+
+const String uniqueTaskName = "geofencingTask";
 
 void main() {
   WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  Workmanager().initialize(callbackDispatcher, isInDebugMode: true);
+  Workmanager().registerPeriodicTask("1", uniqueTaskName,
+      frequency: Duration(seconds: 15));
   runApp(const Main());
+}
+@pragma('vm:entry-point')
+void callbackDispatcher() {
+  Workmanager().executeTask((task, inputData) async {
+    switch (task) {
+      case uniqueTaskName:
+        await checkGeofenceStatus();
+        break;
+    }
+    return Future.value(true);
+  });
+}
+
+Future<void> checkGeofenceStatus() async {
+  Position position = await Geolocator.getCurrentPosition(
+      desiredAccuracy: LocationAccuracy.high);
+  print(position);
+  print("============================");
 }
 
 class Main extends StatefulWidget {
@@ -134,13 +159,16 @@ class _MainState extends State<Main> {
     // ]);
     // First get the FlutterView.
     FlutterView view = WidgetsBinding.instance.platformDispatcher.views.first;
-    print("SCREEN WIDTH IS: ${view.display.size.width}; SCREEN HEIGHT IS: ${view.display.size.height}");
+    print(
+        "SCREEN WIDTH IS: ${view.display.size.width}; SCREEN HEIGHT IS: ${view.display.size.height}");
     // 1560 + 720
     if (view.display.size.width + view.display.size.height >= 2560 + 1600) {
       globals.scaleParam = (view.display.size.shortestSide / 720) * 0.3;
-    } else if (view.display.size.width + view.display.size.height >= 1920 + 1080) {
+    } else if (view.display.size.width + view.display.size.height >=
+        1920 + 1080) {
       globals.scaleParam = (view.display.size.shortestSide / 720) * 0.3;
-    } else if (view.display.size.width + view.display.size.height >= (1560 + 720)) {
+    } else if (view.display.size.width + view.display.size.height >=
+        (1560 + 720)) {
       globals.scaleParam = (view.display.size.shortestSide / 720) * 0.4;
     } else {
       globals.scaleParam = 0.5;
@@ -166,13 +194,16 @@ class _MainState extends State<Main> {
           primary: Colors.black,
           onPrimary: Colors.white,
           onError: Colors.white,
-          secondary: Colors.black38, // TODO: Change this later? To make more sense with black/white style
+          secondary: Colors
+              .black38, // TODO: Change this later? To make more sense with black/white style
           onSecondary: Colors.black,
         ),
         useMaterial3: true,
         brightness: Brightness.light,
-        pageTransitionsTheme: const PageTransitionsTheme(
-            builders: {TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(), TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder()}),
+        pageTransitionsTheme: const PageTransitionsTheme(builders: {
+          TargetPlatform.android: FadeUpwardsPageTransitionsBuilder(),
+          TargetPlatform.iOS: FadeUpwardsPageTransitionsBuilder()
+        }),
         bottomNavigationBarTheme: const BottomNavigationBarThemeData(),
         scaffoldBackgroundColor: Colors.white,
         appBarTheme: AppBarTheme(
@@ -182,14 +213,16 @@ class _MainState extends State<Main> {
           surfaceTintColor: Colors.white,
           elevation: 4,
           scrolledUnderElevation: 4,
-          titleTextStyle: GoogleFonts.inter(fontSize: 24, fontWeight: FontWeight.w700, color: Colors.black),
+          titleTextStyle: GoogleFonts.inter(
+              fontSize: 24, fontWeight: FontWeight.w700, color: Colors.black),
           // backgroundColor: Colors.white,
           // shadowColor: Colors.grey.withOpacity(0.2),
           // foregroundColor: Colors.black
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
-            shape: const RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(10))),
+            shape: const RoundedRectangleBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10))),
             backgroundColor: Colors.black,
             // backgroundColor: Color(0xFFFFCA3C),
             foregroundColor: Colors.white,
